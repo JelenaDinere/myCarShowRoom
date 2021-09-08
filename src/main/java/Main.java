a3dfcd23586c00f0776c41f53adb11c07331d19c
Jelena Dinere <jelena.dinere1@gmail.com>
2021-09-08 21:30:30 +0300

    Initial Commit after assignment is done

---
+++ src/main/java/Main.java
@@ -0,0 +1,108 @@
+import cars.Car;
+import cars.Showroom;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.UUID;
+
+public class Main {
+    Showroom showroom = new Showroom();
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    Main main = new Main();
+    main.showMenu();
+    }
+
+    void showMenu(){
+
+        String userInput = "";
+        do {
+            System.out.println("Welcome to the showroom, please choose activity: ");
+            System.out.println("1. Add Car");
+            System.out.println("2. View All Cars");
+            System.out.println("3.View Single Car");
+            System.out.println("4. Remove Car");
+            System.out.println("\nEnter Quit to end program...");
+
+            System.out.println("Choose a number:");
+            userInput = scanner.nextLine();
+
+            switch (userInput) {
+                case "Quit":
+                    System.out.println("Exiting Application...");
+                    break;
+                    //addCar
+                case "1":
+                    addCar();
+                    break;
+                    //view all cars
+                case "2":
+                    viewAllCars();
+                    break;
+                    //view single car
+                case"3":
+                    viewSingleCar();
+                    break;
+                case"4":
+                    removeCar();
+                    break;
+                default:
+                    break;
+            }
+        } while(!userInput.equalsIgnoreCase("Quit"));
+            return;
+        }
+        void addCar(){
+
+            System.out.println("Add Car");
+            Car car = new Car();
+
+            System.out.println("Enter Name:");
+            car.name=scanner.nextLine();
+
+            System.out.println("Enter Manufacturer");
+            car.manufacturer=scanner.nextLine();
+
+            System.out.println("Enter Type:");
+            car.type =scanner.nextLine();
+
+            car.id = UUID.randomUUID();
+            String message =showroom.addCar(car);
+            System.out.println(message);
+
+    }
+    void viewAllCars(){
+        ArrayList<Car> allCars = showroom.getAllCars();
+        System.out.println("All Available Cars");
+        System.out.println("Car Name \t Manufacturer \t Car Type");
+        for(Car car:allCars){
+            System.out.println(car.name  + '\t'+ car.manufacturer + '\t'+ car.type);
+        }
+    }
+    void viewSingleCar() {
+        System.out.println("View single");
+        System.out.println("Please provide car Index");
+
+        int carIndex = scanner.nextInt();
+        String message = showroom.getSingleCar(carIndex);
+        System.out.println(message);
+//        Car car = showroom.getSingleCar(carIndex);
+//        System.out.println("Car Number"+ car.id);
+//        System.out.println("Car Name"+ car.name);
+//        System.out.println("Car Type"+ car.type);
+
+    }
+    void removeCar(){
+
+        System.out.println("Enter car ID to remove");
+        int carIndex = scanner.nextInt();
+        String car =showroom.removeCar(carIndex);
+        System.out.println(car);
+
+
+
+
+    }
+}
---
+++ src/main/java/cars/Showroom.java
@@ -0,0 +1,39 @@
+package cars;
+
+import java.util.ArrayList;
+
+public class Showroom {
+    private ArrayList<Car>cars =new ArrayList<Car>();
+    //add Car
+    public String addCar(Car car){
+        this.cars.add(car);
+        return car.name + "\nAdded successfully";
+    }
+    //get AllClass;
+    public ArrayList<Car> getAllCars() {
+
+        return cars;
+    }
+    //get SingleCar;
+    //public Car getSingleCar(int carIndex){
+      //  return this.cars.get(carIndex);
+    public String getSingleCar(int carIndex){
+        try {
+            Car car = this.cars.get(carIndex);
+            return "Car Number: " + car.id + "\n Car Manufacturer" + car.manufacturer + "\n Car Type" + car.type;
+        }catch (Exception ex){
+            return "Unable to find car.Please try again with valid index!!!";
+        }
+    }
+    //removeCar
+    public String removeCar(int carIndex) {
+        try {
+            cars.remove(carIndex);
+        }catch (Exception exc){
+            return "Unable to remove specified car";
+    }
+        {
+            return "Car removed";
+        }
+    }
+}
